db3a83174dc15dd3c5c5cc8f0c8f6479eff19a15
Cassidy <cassidysm@bluelinedesigners.onmicrosoft.com>
2025-05-06 20:12:18 -0500

    init

--- client/src/App.vue
+++ client/src/App.vue
@@ -3,8 +3,6 @@
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </nav>
-  <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+  <h1 class="text-3xl font-bold underline">Hello world!</h1>
   <router-view />
 </template>
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import { fileURLToPath, URL } from 'node:url';
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd());
